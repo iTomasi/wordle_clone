9cd4b90634d6bf73ef3f750c8deab8067aac2510
Tomas Duclos <duclosiw@gmail.com>
2022-02-07 03:22:53 -0300

    adding more fields and improving input (password type) && installing react-icons

--- client/src/components/form/Input.tsx
+++ client/src/components/form/Input.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { LinksFunction } from "remix";
 import inputCss from "~/css/components/form/input.css";
+
+// Components
+import { HiEye, HiEyeOff } from "react-icons/hi";
 
 interface IInputProps {
     className?: string;
@@ -21,18 +24,44 @@ export const links: LinksFunction = () => {
 }
 
 const Input = ({ className = "", type = "text", placeholder, labelTitle, name, isValid = true, errorMessage = "", onChange, onBlur }: IInputProps) => {
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+
     return (
         <div className={`iw_input ${className}`}>
             <label className="text-base">{labelTitle}</label>
 
-            <input
-                className={`iw_text-white text-base ${!isValid ? "mb-2 iw_error" : ""}`}
-                type={type}
-                placeholder={placeholder}
-                name={name}
-                onChange={onChange}
-                onBlur={onBlur}
-            />
+            {
+                type === "password"
+                ? (
+                    <label className={`iw_theInputLabel ${!isValid ? "mb-2 iw_error" : ""}`}>
+                        <input
+                            className="iw_theInputPassword iw_text-white text-base"
+                            type={showPassword ? "text" : "password"}
+                            placeholder={placeholder}
+                            name={name}
+                            onChange={onChange}
+                            onBlur={onBlur}
+                        />
+
+                        <div className="iw_inputIconContainer" onClick={() => setShowPassword((prev) => !prev)}>
+                            {
+                                showPassword
+                                    ? <HiEye className="iw_inputIcon"/>
+                                    : <HiEyeOff className="iw_inputIcon"/>
+                            }
+                        </div>
+                    </label>
+                ) : (
+                    <input
+                        className={`iw_theInput iw_text-white text-base ${!isValid ? "mb-2 iw_error" : ""}`}
+                        type={type}
+                        placeholder={placeholder}
+                        name={name}
+                        onChange={onChange}
+                        onBlur={onBlur}
+                    />
+                )
+            }
 
             {
                 (!isValid && errorMessage) && (
--- client/src/routes/auth/sign-up.tsx
+++ client/src/routes/auth/sign-up.tsx
@@ -26,6 +26,23 @@ const AuthSignUp = () => {
             blurMode: true,
             regExp: new RegExp(/^[A-Za-z0-9]{3,30}$/),
             errorMessage: "Your username should contain between 3-30 characters (characters allowed: a-z 0-9)"
+        },
+        email: {
+            isValid: true,
+            blurMode: true,
+            regExp: new RegExp(/^[A-Za-z0-9\.\_]+\@[A-Za-z]+\.[A-Za-z]{2,4}$/),
+            errorMessage: "Wrong email, example: wordleapp@app.com"
+        },
+        password: {
+            isValid: true,
+            blurMode: true,
+            regExp: new RegExp(/^.{5,}$/),
+            errorMessage: "Your password should contains at least 5 characters"
+        },
+        confirm_password: {
+            isValid: true,
+            blurMode: true,
+            errorMessage: "Your password not match"
         }
     })
 
@@ -33,7 +50,7 @@ const AuthSignUp = () => {
         const targetName = e.target.name;
 
         if (!targetName || !inputsValidations[targetName] || !inputsValidations[targetName].blurMode) return
-        
+
         setInputsValidations((prev: any) => {
             return {
                 ...prev,
@@ -65,7 +82,7 @@ const AuthSignUp = () => {
     return (
         <form className="iw_form">
             <Input
-                className="mb-4"
+                className="mb-8"
                 labelTitle="Name"
                 placeholder="Your name"
                 name="name"
@@ -76,6 +93,7 @@ const AuthSignUp = () => {
             />
 
             <Input
+                className="mb-8"
                 labelTitle="Username"
                 placeholder="ex. Tomas"
                 name="username"
@@ -83,6 +101,38 @@ const AuthSignUp = () => {
                 errorMessage={inputsValidations.username.errorMessage}
                 onBlur={handleOnBlurInputs}
                 onChange={handleOnChangeInputs}
+            />
+
+            <Input
+                className="mb-8"
+                labelTitle="Email"
+                placeholder="ex. wordleclone@app.com"
+                name="email"
+                isValid={inputsValidations.email.isValid}
+                errorMessage={inputsValidations.email.errorMessage}
+                onBlur={handleOnBlurInputs}
+                onChange={handleOnChangeInputs}
+            />
+
+            <Input
+                className="mb-8"
+                type="password"
+                labelTitle="Password"
+                placeholder="*****"
+                name="password"
+                isValid={inputsValidations.password.isValid}
+                errorMessage={inputsValidations.password.errorMessage}
+                onBlur={handleOnBlurInputs}
+                onChange={handleOnChangeInputs}
+            />
+
+            <Input
+                type="password"
+                labelTitle="Confirm Password"
+                placeholder="*****"
+                name="confirm_password"
+                isValid={inputsValidations.confirm_password.isValid}
+                errorMessage={inputsValidations.confirm_password.errorMessage}
             />
 
         </form>
