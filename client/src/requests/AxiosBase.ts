5a3a130547ad74221863d722083baa960f8db772
Tomas Duclos <duclosiw@gmail.com>
2022-03-06 23:42:14 -0300

    fix axiosbase

--- client/src/requests/AxiosBase.ts
+++ client/src/requests/AxiosBase.ts
@@ -1,9 +1,7 @@
 import Axios from "axios";
 
-const API_URL =
-    process.env.NEXT_PUBLIC_API_URL
-        ? process.env.NEXT_PUBLIC_API_URL
-        : "http://192.168.1.88:4000"
+//const API_URL = "http://192.168.1.88:4000"
+const API_URL = "https://wordle-backend-livid.vercel.app"
 
 export const AxiosApi = Axios.create({
     baseURL: `${API_URL}/api`
