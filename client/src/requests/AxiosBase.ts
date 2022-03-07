605f70178785991f8388bf96df0241860bcf8d71
Tomas Duclos <duclosiw@gmail.com>
2022-03-06 23:07:53 -0300

    axiosbase envioroment

--- client/src/requests/AxiosBase.ts
+++ client/src/requests/AxiosBase.ts
@@ -1,5 +1,10 @@
 import Axios from "axios";
 
+const API_URL =
+    process.env.NEXT_PUBLIC_API_URL
+        ? process.env.NEXT_PUBLIC_API_URL
+        : "http://192.168.1.88:4000"
+
 export const AxiosApi = Axios.create({
-    baseURL: "http://192.168.1.88:4000/api"
+    baseURL: `${API_URL}/api`
 })
